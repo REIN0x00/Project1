56cb91dc375d8d2f27356545fcf44e675a9904c2
S67 <81880212+hollow321@users.noreply.github.com>
2023-02-22 21:28:56 +0900

    GameTutorial

--- Project1/OCursor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class OCursor{
-public:
-	int cursorX=0, cursorY=0;
-	int ScanCursor(int X,int Y);
-};
-
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,47 @@
 #include <conio.h>
 #include <stdio.h>
 
-#include "OStage.h"
 
 #define BOARD_WIDTH 8
 #define BOARD_HEIGHT 8
 
+int cursorX, cursorY;
+
+
 int main(){
-	OStage OStage;
-	OStage.MakeStage(BOARD_WIDTH, BOARD_HEIGHT);
-	_getch();
+	while (1) {
+		//XY軸作成
+		for (int y = 0; y < BOARD_HEIGHT; y++) {
+			for (int x = 0; x < BOARD_WIDTH; x++) {
+				//カーソル位置特定
+				if ((y == cursorY) && (x == cursorX)) {
+					printf("◎");
+				}
+				else {
+					printf("・");
+				}
+			}
+			printf("\n");
+		};
+		switch (_getch()){
+			case 'w':
+				cursorY--;
+				break;
+			case 's':
+				cursorY++;
+				break;
+			case 'a':
+				cursorX--;
+				break;
+			case 'd':
+				cursorX++;
+				break;
+		default:
+			break;
+		}
+
+
+	}
+
 	
 }
